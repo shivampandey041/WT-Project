ee6eea6dfd0d73622c4b5a5fff496c040695dca1
Shivam Pandey <69375255+shivampandey041@users.noreply.github.com>
2020-09-16 21:13:24 +0530

    Add files via upload

---
+++ WT-Project/js/main.js
@@ -0,0 +1,29 @@
+var btnn=document.getElementById("click");
+var more=document.getElementById("more-stu");
+
+more.style.display='none';
+function hideCS() {
+    if (more.style.display != 'none') {
+        more.style.display = 'none';
+
+    }
+    else {
+        more.style.display = 'block';
+    }
+}
+
+
+
+var btnnn=document.getElementById("click1");
+var moree=document.getElementById("more-stu1");
+
+moree.style.display='none';
+function hideET() {
+    if (moree.style.display != 'none') {
+        moree.style.display = 'none';
+
+    }
+    else {
+        moree.style.display = 'block';
+    }
+}
